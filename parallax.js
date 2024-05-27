1151476ff22c4fcb3ff05053981adce3bca33627
gershomog <gershomog@gmail.com>
2024-05-27 18:37:10 -0400

    newest changes

--- parallax.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll', function() 
-{
-  const scrolled = window.scrollY;
-  const parallaxContent = document.querySelector('.parallax-content');
-  parallaxContent.style.transform = `translateY(${scrolled}px)`;
-});
